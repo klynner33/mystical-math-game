7bd020762aef6f0a5747f90939e654e86b8ff9bc
klynner33 <113389068+klynner33@users.noreply.github.com>
2026-01-09 14:40:12 -0600

    Added images and responsive styling for main screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 export default function Footer() {
-
-    return (
-      <div className="footer">
-        <p className="copyright">© 2025 Mystical Math. All rights reserved.</p>
-        <p className="credit">Game ideas and testing by Lettie, Grace, & Miles</p>
-        <p className="credit">Characters and items by TrulyMalicious — CC-BY 4.0</p>
-      </div>
-    );
+  return (
+    <div className="footer">
+      <p className="copyright">© 2026 Mystical Math. All rights reserved.</p>
+      <p className="credit">Game ideas and testing by Lettie, Grace, & Miles</p>
+      <p className="credit">
+        Characters and items by TrulyMalicious — CC-BY 4.0
+      </p>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
 import Button from "../components/Button";
 import ChooseGameModal from "../components/ChooseGameModal";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
+import KittyImg from "../assets/mystical-math-kitty.png";
+import TitleImg from "../assets/mystical-math-title.png";
 
 export default function Home() {
-    const [chooseGameModal, setChooseGameModal] = useState("modal-hidden");
+  const [chooseGameModal, setChooseGameModal] = useState("modal-hidden");
 
-    function openChooseGameModal() {
-        setChooseGameModal("modal");
-    }
-    
+  function openChooseGameModal() {
+    setChooseGameModal("modal");
+  }
+
   return (
     <div className="main-screen">
+      <div className="mobile-hero">
+        <div className="title-img-container">
+          <img src={TitleImg} alt="Mystical Math Title" className="title-img" />
+        </div>
+        <div className="kitty-img-container">
+          <img src={KittyImg} alt="Mystical Math Kitty" className="kitty-img" />
+        </div>
+      </div>
       {chooseGameModal === "modal-hidden" && (
         <div className="play-button">
           <Button
@@ -31,4 +41,4 @@ export default function Home() {
       <Footer />
     </div>
   );
-};
+}
